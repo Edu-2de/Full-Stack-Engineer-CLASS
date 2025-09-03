af2528023e7cd41397641febaa2147103cd1e490
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-03 14:21:02 -0300

    Add variable manipulations and console logs for levelUp, powerLevel, multiplyMe, quarterMe, gainedDollar, and lostDollar

--- BuildingInteractiveWebsites/JavaScriptSyntaxPart_I/variables.js
+++ BuildingInteractiveWebsites/JavaScriptSyntaxPart_I/variables.js
@@ -24,3 +24,37 @@ const entree = 'Enchiladas';
 console.log(entree);
 //tenta mudar o valor da variavel para outra, mas da erro por ser const
 entree = 'Tacos';
+
+
+//dfine as variaveis com valores inicias
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+//altera o valor das variaveis 
+
+//soma 5
+levelUp += 5
+//diminui 5
+powerLevel -= 100
+//multiplica 11
+multiplyMe *= 11
+//divide por 4
+quarterMe /= 4
+
+//imprime o novo valor das variaveis
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+
+//dfine as variaveis com valores inicias
+let gainedDollar = 3;
+let lostDollar = 50;
+
+//aumenta em um o valor da variavel em um, ou seja 4
+gainedDollar ++;
+//diminui em um o valor da variavel em um, ou seja 49
+lostDollar --;
