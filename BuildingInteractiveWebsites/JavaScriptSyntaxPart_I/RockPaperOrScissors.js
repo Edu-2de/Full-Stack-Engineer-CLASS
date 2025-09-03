c58bfe8050bb72bb16051e811147dfca61e9a58a
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-03 15:22:28 -0300

    Refactor getComputerChoice function for clarity and formatting improvements

--- BuildingInteractiveWebsites/JavaScriptSyntaxPart_I/RockPaperOrScissors.js
+++ BuildingInteractiveWebsites/JavaScriptSyntaxPart_I/RockPaperOrScissors.js
@@ -16,17 +16,32 @@ const getUserChoice = (userInput)=>{
   }
 }
 
+//faz uma funcao de pegar a escolha do computador
 const getComputerChoice = ()=>{
+
+  //cria uma variavel mutavel com um numero aletorio, de 0 a 2
   let choice = Math.floor(Math.random() * 3);
+
+  //se for 1 a choice, entao sera rock
   if (choice == 1){
     choice = 'rock'
+
+  //se for 2 a choice, entao sera paper
   }else if(choice == 2){
      choice ='paper'
+
+  //se nao for nem 1 nem 2, ele sera scissors 
   }else{
       choice = 'scissors'
   }
   return choice;
 }
+
+//aqui ele definea esocolha do usuario
 let userInput = getUserChoice('rock'); 
+
+//chama a funcao para a escolha do pc
 let choice = getComputerChoice();
+
+//mostra no console tanto a escolha do usuario quanto do pc
 console.log(`Voce escolheu${userInput} e a máquina escolheu ${choice}`)
