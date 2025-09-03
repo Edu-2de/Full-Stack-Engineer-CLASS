bfd6caffc6e4a962c93ee3a60ccd516460165132
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-03 14:24:48 -0300

    Refactor variables.js to improve formatting, add missing newlines, and enhance console log outputs for clarity.

--- BuildingInteractiveWebsites/JavaScriptSyntaxPart_I/variables.js
+++ BuildingInteractiveWebsites/JavaScriptSyntaxPart_I/variables.js
@@ -5,9 +5,11 @@ var favoriteFood = 'pizza';
 var numOfSlices = 8;
 
 
+
 //imprime as variaveis
 console.log(favoriteFood);
 console.log(numOfSlices);
+
 
 
 //define a variavel
@@ -18,12 +20,14 @@ changeMe = false;
 console.log(changeMe);
 
 
+
 //define a variavel const que nao pode ser alterada
 const entree = 'Enchiladas';
 //imprimi a variavel
 console.log(entree);
 //tenta mudar o valor da variavel para outra, mas da erro por ser const
 entree = 'Tacos';
+
 
 
 //dfine as variaveis com valores inicias
@@ -33,7 +37,6 @@ let multiplyMe = 32;
 let quarterMe = 1152;
 
 //altera o valor das variaveis 
-
 //soma 5
 levelUp += 5
 //diminui 5
@@ -50,6 +53,7 @@ console.log('The value of multiplyMe:', multiplyMe);
 console.log('The value of quarterMe:', quarterMe);
 
 
+
 //dfine as variaveis com valores inicias
 let gainedDollar = 3;
 let lostDollar = 50;
@@ -58,3 +62,32 @@ let lostDollar = 50;
 gainedDollar ++;
 //diminui em um o valor da variavel em um, ou seja 49
 lostDollar --;
+
+
+
+//define a variavel como cat
+var favoriteAnimal = 'cat'
+//imprime o valor da variavel, ou seja cat
+console.log('My favorite animal: ' + favoriteAnimal);
+
+
+
+//define as variaveis
+var myName = 'Lucas'
+var myCity = 'New York'
+//imprime as variaveis usando ${} que é um jeito de mostrar direto no console.log, sem precisar do +
+console.log(`My name is ${myName}.My favorite city is ${myCity}.`);
+
+
+
+//define as variaveis
+let newVariable = 'Playing around with typeof.';
+//imprime o typo da variavel, ou seja string nessse caso
+console.log(typeof newVariable)
+//muda o tipo de variavel para o tipo int, number
+newVariable = 1;
+//imprime o novo tipo da variavel
+console.log(typeof newVariable)
+
+
+
