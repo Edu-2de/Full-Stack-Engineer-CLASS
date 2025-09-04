30224af19cead64079cc508339cf2f676385a7a1
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-04 12:11:34 -0300

    Add initial implementation of arrays for New Year's resolutions and hobbies

---
+++ BuildingInteractiveWebsites/JavaScriptSyntaxPart_II/Arrays/Arrays.js
@@ -0,0 +1,13 @@
+//// cSpell:disable
+
+//cria uma array em uma variavel chamada newYearsResolutions
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+//printa a variavel, ou seja prina o array
+console.log(newYearsResolutions);
+
+
+
+//cria uma array em uma variavel chamada hobbies
+const hobbies = ['oi ', ' tudo', 'bem ']
+//printa a variavel, ou seja prina o array
+console.log(hobbies)
