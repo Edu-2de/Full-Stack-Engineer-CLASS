dbc55dc828cc55f4d8dbc0dbbad2f8c8879b10e8
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-04 12:35:59 -0300

    Refactor and restore Arrays.js and SecretMessage.js files with initial array definitions and manipulations

---
+++ BuildingInteractiveWebsites/JavaScriptSyntaxPart_II/2.Loops/Loops.js
@@ -0,0 +1,27 @@
+// cSpell:disable
+
+// cria um array em uma variavel chamada vacationSpots
+let vacationSpots = ['new York', 'sao Paulo', 'buenos Aires']
+//imprime o 1, 2 e 3 item da array
+console.log(vacationSpots[0])
+console.log(vacationSpots[1])
+console.log(vacationSpots[2])
+
+
+
+//cria um loop que comeca em 5 e caba no 11, diz que toda vez que vai passar pelo loop, ele aumenta em um o contador
+for(let counter = 5; counter <= 11; counter++){
+  //imprime o contador toda vez, o que fara ele printar de 5 ate 11
+  console.log(counter);
+}
+
+
+
+//cria um loop que comeca em 3 e vai ate 0, so que dessa vez diminuindo o contador, contagem regressiva
+for (let counter = 3; counter >= 0; counter--) {
+  //imprime de 3 a 0
+  console.log(counter);
+}
+
+
+
