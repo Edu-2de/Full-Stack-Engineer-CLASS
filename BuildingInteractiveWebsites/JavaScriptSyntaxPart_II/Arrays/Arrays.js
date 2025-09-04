6a26364ece9339d0c653c6ff65d6714443fba0c8
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-04 12:17:19 -0300

    Fix formatting issues in array declarations and console logs for consistency

--- BuildingInteractiveWebsites/JavaScriptSyntaxPart_II/Arrays/Arrays.js
+++ BuildingInteractiveWebsites/JavaScriptSyntaxPart_II/Arrays/Arrays.js
@@ -8,25 +8,25 @@ console.log(newYearsResolutions);
 
 
 //cria uma array em uma variavel chamada hobbies
-const hobbies = ['oi ', ' tudo', 'bem ']
+const hobbies = ['oi ', ' tudo', 'bem '];
 //printa a variavel, ou seja prina o array
-console.log(hobbies)
+console.log(hobbies);
 
 
 
 //cria uma array em uma variavel chamada famousSayings
 const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
 //cria uma variavel que armazena o primeiro item da array, ou seja 'Fortune favors the brave.'
-const listItem = famousSayings[0]
+const listItem = famousSayings[0];
 //printa o 4 item do array, ou seja vai dar undefined, pois o array so tem 2 itens, pois comeca no 0 ate o 2
-console.log(famousSayings[3])
+console.log(famousSayings[3]);
 
 
 
 //cria uma array em uma variavel chamada groceryList
 let groceryList = ['bread', 'tomatoes', 'milk'];
 //define que o segundo item do array vai ser avocados
-groceryList[1] = 'avocados'
+groceryList[1] = 'avocados';
 
 
 
@@ -50,3 +50,10 @@ utensils[3] = 'Spoon';
 //imprime o array utensils
 console.log(utensils);
 
+
+
+
+//cria uma array em uma variavel chamada objectives
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+//imrpime a quantidade de itens que tem o array, ou seja 3
+console.log(objectives.length)
