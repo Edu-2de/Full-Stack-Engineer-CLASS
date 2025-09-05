9bb3bcf99823306caaf44f5e4dbd84d41a374c12
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-05 14:05:44 -0300

    Refactor Dog class comments for clarity and consistency

--- BuildingInteractiveWebsites/JavaScriptSyntaxPart_III/Classes.js
+++ BuildingInteractiveWebsites/JavaScriptSyntaxPart_III/Classes.js
@@ -1,36 +1,35 @@
 // cSpell:disable
 
-//cria a classe dog que tem um constructor para um objeto da classe dog, com o name e o behavior
+// cria a classe Dog que tem um constructor para instanciar um objeto com name e behavior
 class Dog {
   constructor(name) {
     this._name = name;
     this._behavior = 0;
   }
-
-  //funcao getter para pegar o name e o behavior, ja que ele é privado
+  // getters para acessar name e behavior (como se fossem "propriedades privadas")
   get name() {
     return this._name;
   }
   get behavior() {
     return this._behavior;
   }   
-  //funcao que aumenta em um o valor de behavior
+  // método que aumenta em 1 o valor de behavior
   incrementBehavior() {
     this._behavior ++;
   }
 }
-//cria um novo objeto de dog, Halley
+// cria um novo objeto da classe Dog chamado Halley
 const halley = new Dog('Halley');
-console.log(halley.name); // Print name value to console
-console.log(halley.behavior); // Print behavior value to console
-halley.incrementBehavior(); // Add one to behavior
-console.log(halley.name); // Print name value to console
-console.log(halley.behavior); // Print behavior value to console
+console.log(halley.name); // imprime o valor de name
+console.log(halley.behavior); // imprime o valor de behavior
+halley.incrementBehavior(); // incrementa behavior em 1
+console.log(halley.name); // imprime novamente name
+console.log(halley.behavior); // imprime novamente behavior
 
 
 
 
-
+// cria a classe Surgeon que precisa de name e department para instanciar um objeto
 class Surgeon{
   constructor(name, department){
     this.name = name;
