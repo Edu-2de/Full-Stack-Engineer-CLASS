759063f535870238918161f613bd97d900bc840e
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-09 11:14:29 -0300

    Add initial test suite for order placement functionality

---
+++ BuildingInteractiveWebsites/JavaScriptTesting/2.AutomateOrganizeTests/AutomateOrganizeTests/AutomateOrganizeTests.js
@@ -0,0 +1,12 @@
+// cSpell:disable
+
+//cria um gupo de testes, nesse caso o nome seria matematica
+describe('Math', () =>{
+  //cria outro grupo dentrod e matematica para os testes de minimo, que tera os testes equivalentes a isso
+  describe('.min', () =>{
+    //cria um teste que diz  que deve retornar o argumento com menor valor
+    it('returns the argument with the lowest value' , () =>{
+      
+    })
+  })
+})
