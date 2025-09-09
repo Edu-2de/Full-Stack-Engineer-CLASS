1388762107834374f7fa5b7d6e96a0dbaa002cca
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-09 11:24:40 -0300

    Add tests for Array.prototype.pop and appendFileSync functionality

--- BuildingInteractiveWebsites/JavaScriptTesting/2.AutomateOrganizeTests/AutomateOrganizeTests/AutomateOrganizeTests.js
+++ BuildingInteractiveWebsites/JavaScriptTesting/2.AutomateOrganizeTests/AutomateOrganizeTests/AutomateOrganizeTests.js
@@ -24,6 +24,77 @@ describe('+', () => {
     // se 3 + 4 === 7 -> passa
     // se não, o teste falha
     assert.ok(3 + 4 === 7);
-
   });
+});
+
+
+
+
+//importa o asset
+const assert = require('assert');
+// cria um array
+const arr = ['entry1', 'entry2'];
+//cria um grupo de testes para o array
+describe('Array.prototype.pop [naive]', () => {
+  //cria um teste que deve retornar o ultimo elemnto do array
+  it('returns the last element in the array [naive]', () => {
+    //se retornar vair aparecer true se nao false
+    assert.ok(pop(['padawan', 'knight']) === 'knight'); 
+  });
+});
+
+// faz o mesmo que acima so que de um jeito diferente
+describe(arr.pop(), () => {
+  it('returns the last element in the array [3phase]', () => {
+    const knightString = 'knight';
+    const jediPath = ['padawan', knightString];
+    const popped = pop(jediPath);
+    assert.ok(popped === knightString);
+  });
+});
+
+
+
+
+
+const assert = require('assert');
+const fs = require('fs');
+let path, str;
+ 
+describe('appendFileSync', () => {
+  it('creates a new file with a string of text', () => {
+ 
+   // Setup
+   path = './message.txt';
+   str = 'Hello Node.js';
+  
+   // Exercise: write to file
+   fs.appendFileSync(path, str);
+ 
+   // Verify: compare file contents to string
+   const contents = fs.readFileSync(path);
+   assert.equal(contents.toString(), str);
+ 
+   // Teardown: restore file
+   fs.unlinkSync(path);
+
+ });
+ 
+ it('creates a new file with a string of text', () => {
+ 
+   // Setup
+   path = './message.txt';
+   str = '';
+  
+   // Exercise: write to file
+   fs.appendFileSync(path, str);
+ 
+   // Verify: compare file contents to string
+   const contents = fs.readFileSync(path);
+   assert.equal(contents.toString(), str);
+ 
+   // Teardown: restore file
+   fs.unlinkSync(path);
+
+ });
 });
