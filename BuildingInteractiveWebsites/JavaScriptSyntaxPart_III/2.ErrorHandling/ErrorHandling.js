ff97cf9b38f0790640747947d38be6c9b46fc32e
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-09 10:48:44 -0300

    Refactor error handling example with additional comments and console logs

--- BuildingInteractiveWebsites/JavaScriptSyntaxPart_III/2.ErrorHandling/ErrorHandling.js
+++ BuildingInteractiveWebsites/JavaScriptSyntaxPart_III/2.ErrorHandling/ErrorHandling.js
@@ -1,1 +1,16 @@
 // cSpell:disable
+
+//Imprime uma mensagem dizendo que a mesagem sera imprimida no console
+console.log('This message will be printed to the console.');
+//funcao que vai dar um erro, pois retorna uma variavel nao definida
+function throwError () {
+  return notDefinedVar;
+}
+//chama a funcao que vai dar um erro no console
+throwError();
+//Por causa do erro so a mensagem de cima sera imprimida, essa nao vai
+console.log('Because of the error, this will not be printed!');
+
+
+
+
